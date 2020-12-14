0a4ba6ad003bd6c4a637ed95d939a16f16e677d5
Pierre Duveau <pierre.duveau@cpe.fr>
2020-12-14 13:56:26 +0100

    remove parasites files in client

--- client/src/utils/config/packetprocessor.h
+++
@@ -1,9 +0,0 @@
-#ifndef PACKETPROCESSOR_h
-#define PACKETPROCESSOR_h
-
-#include "../includes/packet.h"
-
-
-void process_packet(Packet *packet);
-
-#endif /* PACKETPROCESSOR_h */
--- client/src/utils/config/serialize.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERIALIZE_H
-#define SERIALIZE_H
-
-#include "../includes/packet.h"
-
-Packet *deserialize(char *buffer_in, char *buffer_size);
-Packet *serializeData(enum action code , void* object);
-Packet *serializeMessage(enum action code, char *message);
-#endif /* SERIALIZE_H */
