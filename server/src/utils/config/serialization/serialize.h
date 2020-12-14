850622a4df5aebafb4aa9072cc5a4bd8ca6c19ab
Pierre Duveau <pierre.duveau@cpe.fr>
2020-12-14 13:51:15 +0100

    remove parasyte files

--- server/src/utils/config/serialization/serialize.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERIALIZE_H
-#define SERIALIZE_H
-
-#include "../../models/packetModel.h"
-
-Packet *Deserialize(char *buffer_in, int buffer_size);
-Packet *SerializeData(enum action code , void* object, int object_size);
-Packet *SerializeMessage(enum action code, char *message);
-#endif /* SERIALIZE_H */
